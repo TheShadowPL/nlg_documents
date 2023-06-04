58d0d3469cc799c3f62b68dc29b21c947f3f8310
Krystian Pasieka <s38131@s.pwste.edu.pl>
2023-06-04 23:50:49 +0200

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'bodacious'
+game 'gta5'
+lua54 'yes'
+
+name "nlg_documents"
+description 'NextLevel Gaming document system'
+author "TheShadow#4691 & qbisz#5187"
+
+
+
+version '1.0'
+
+
+
+client_scripts {
+    'Config.lua',
+    'client/client.lua'
+}
+
+server_scripts {
+	'Config.lua',
+	'@oxmysql/lib/MySQL.lua',
+    'server/server.lua'
+}
+
+shared_script '@ox_lib/init.lua'
