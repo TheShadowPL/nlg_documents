73bf877256c8985fe22ce466e46dbc2e8b6402f7
Krystian Pasieka <s38131@s.pwste.edu.pl>
2023-06-05 00:45:10 +0200

    ESX framework initialization

---
+++ client/client.lua
@@ -0,0 +1,9 @@
+ESX = nil
+local PlayerData = {}
+local isDead = false
+while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(100)
+    PlayerData = ESX.GetPlayerData()
+
+end
