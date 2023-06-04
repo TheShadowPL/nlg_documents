e101932df12fca58a96fb0e7289c11b0df9ec954
Krystian Pasieka <s38131@s.pwste.edu.pl>
2023-06-05 01:00:32 +0200

    .

--- client/client.lua
+++ client/client.lua
@@ -1,9 +1,37 @@
 ESX = nil
 local PlayerData = {}
 local isDead = false
-while ESX == nil do
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-    Citizen.Wait(100)
-    PlayerData = ESX.GetPlayerData()
+local inwallet = false
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        PlayerData = ESX.GetPlayerData()
+        Citizen.Wait(100)
+    end
+end)
 
-end
+
+RegisterCommand('portfel', function(source, args, rawCommand)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    SendNUIMessage({
+        type = "open",
+        money = xPlayer.getMoney(),
+        bank = xPlayer.getAccount('bank').money,
+        black = xPlayer.getAccount('black_money').money
+    })
+    inwallet = true
+    --SetNuiFocus(false, true)
+    local money = xPlayer.getMoney()
+    local bank = xPlayer.getAccount('bank').money
+    local black = xPlayer.getAccount('black_money').money
+    TriggerClientEvent('esx:showNotification', _source, 'Stan konta: ~g~$'..money..'~s~\nStan konta bankowego: ~b~$'..bank..'~s~\nStan konta brudnego: ~r~$'..black..'~s~')
+    if inwallet == true then
+        SetNuiFocus(true, true)
+    else 
+        SetNuiFocus(false, false)
+        SendNuiMessage({
+            type = "close"
+        })
+    end
+end, false)
