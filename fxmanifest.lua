84ef46cede9ea2da01dda4ac7e5dd712ca15fb9c
Krystian Pasieka <s38131@s.pwste.edu.pl>
2023-06-04 23:52:56 +0200

    fxmanifest update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,7 +10,13 @@ author "TheShadow#4691 & qbisz#5187"
 
 version '1.0'
 
+ui_page "html/index.html"
 
+files {
+    "html/index.html",
+    "html/style.css",
+    "html/script.js"
+}
 
 client_scripts {
     'Config.lua',
